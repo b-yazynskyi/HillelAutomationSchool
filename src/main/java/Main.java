09a22d4c772a905cd4edd1a1e36d44ffd0492df4
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-17 12:20:29 +0300

    Move system.out to try block

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MathOperations mathOperator = new MathOperations();
-        System.out.println("Введіть ціле число: ");
 
         try {
+            System.out.println("Введіть ціле число: ");
             int userNumber = scanner.nextInt();
             int squareOfNumber = mathOperator.squareNumber(userNumber);
             System.out.printf("Квадрат числа %d дорівнює %d%n", userNumber, squareOfNumber);
