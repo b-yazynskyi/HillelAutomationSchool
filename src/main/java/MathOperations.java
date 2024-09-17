7cd59addb3b8bbca57bcc3583b944324a17273b6
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-17 12:01:41 +0300

    Change Math.pow() to own loop because Math.pow() better to use for non-integer numbers.

--- src/main/java/MathOperations.java
+++ src/main/java/MathOperations.java
@@ -8,8 +8,12 @@ public class MathOperations {
         return Math.PI * Math.pow(radius, 2) * high;
     }
 
-    public double elevationOfNumberToPower(int a, int b) {
-        return Math.pow(a, b);
+    public int elevationOfNumberToPower(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
     }
 
 }
