02fc34758859d03dbbf35380a238277ba1e16301
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-17 11:30:47 +0300

    Add first case to Main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MathOperations mathOperator = new MathOperations();
+        System.out.println("Введіть ціле число: ");
+
+        int userNumber = scanner.nextInt();
+        int squareOfNumber = mathOperator.squareNumber(userNumber);
+        System.out.println("Квадрат числа " + userNumber + " дорівнює " + squareOfNumber);
+
+
+
     }
 }
