69a445a494c3278c56ff662b38b2921d323dc758
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-19 20:09:06 +0300

    Create MainUtils class + reaadIntFromConsole method

---
+++ src/main/java/utils/MainUtils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.Scanner;
+
+public final class MainUtils {
+
+    public static int readIntFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextInt();
+    };
+}
