59c51129364a3fca8d1b6ad072605ffea78049ba
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-20 09:46:21 +0300

    Create isPrime method

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -54,4 +54,19 @@ public final class Utils {
             default -> "";
         };
     }
+
+    public static boolean isPrime(int number) {
+
+        if ( number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
