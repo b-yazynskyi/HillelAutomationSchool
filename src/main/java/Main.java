934fcea657c19b4acf4455fff942a2878814501f
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-25 11:40:49 +0300

    Add some improvements to system.out

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,13 @@ import utils.Utils;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Utils.averageValue(Utils.getArrayIntegersFromConsole()));
+        System.out.println("Середнє значення массиву: " + Utils.averageValue(Utils.getArrayIntegersFromConsole()));
 
-        System.out.println(Utils.minValueInArray(Utils.getArrayIntegersFromConsole()));
+        System.out.println("Мінімальне значення масиву: " + Utils.minValueInArray(Utils.getArrayIntegersFromConsole()));
 
-        System.out.println(Utils.maxValueInArray(Utils.getArrayIntegersFromConsole()));
+        System.out.println("Максимальне значення масиву: " + Utils.maxValueInArray(Utils.getArrayIntegersFromConsole()));
 
-        System.out.println(Utils.reverseElementsInArray(Utils.getArrayIntegersFromConsole()));
+        System.out.println("Перевернутий массив: " + Utils.reverseElementsInArray(Utils.getArrayIntegersFromConsole()));
         System.out.println(Utils.isNumberInArray(Utils.getArrayIntegersFromConsole(), Utils.getNumberFromConsole()));
     }
 }
