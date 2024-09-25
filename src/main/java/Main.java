73b157a32c827056e71e294683f37a90a499b6a9
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-25 10:15:54 +0300

    Add realization of minValueInArray and maxValueInArray to main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Utils.averageValue(Utils.getArrayIntegersFromConsole()));
+
+        System.out.println(Utils.minValueInArray(Utils.getArrayIntegersFromConsole()));
+
+        System.out.println(Utils.maxValueInArray(Utils.getArrayIntegersFromConsole()));
     }
 }
