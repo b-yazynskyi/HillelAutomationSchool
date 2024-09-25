76fc49d6ad93fe0ebb9d8b28ca3cf51bf71fde83
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-25 10:07:12 +0300

    Create getArrayInetegersFromConsole method which receive ints from console

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -2,10 +2,29 @@ package utils;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public final class Utils {
 
-    private Utils(){};
+    private Utils(){}
+
+    public static int[] getArrayIntegersFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть розмір масиву");
+
+        int size = scanner.nextInt();
+
+        System.out.println("Введіть " + size + " цілих чисел");
+
+        int[] arrayNumbers = new int[size];
+
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            arrayNumbers[i] += scanner.nextInt();
+        }
+
+        return arrayNumbers;
+    }
 
     public static double averageValue(int[] numbers) {
 
