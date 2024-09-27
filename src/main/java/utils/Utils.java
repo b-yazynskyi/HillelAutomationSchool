66ba747f0d9a7f8e32b8c8ab34ebe7a79b51c114
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-27 10:52:31 +0300

    Create getStringFromUser method

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,11 +1,20 @@
 package utils;
 
+import java.util.Scanner;
+
 public final class Utils {
 
     private Utils(){}
 
-    public static String repeatEnd(String word, int number) {
+    public static String getStringFromUser() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введіть слово");
+
+        return scanner.nextLine();
+    }
+
+    public static String repeatEnd(String word, int number) {
         if (word.length() < number) {
             throw new IllegalArgumentException("Число не може бути більше довжини слова.");
         }
