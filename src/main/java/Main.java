ee7e5daac7ab0fb2b8ccee2070248c0d3fee03de
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-27 10:55:40 +0300

    Change hardcoded values to method which get data from user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import utils.Utils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            System.out.println(Utils.repeatEnd(Utils.getStringFromUser(), Utils.getNumberFromUser()));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
