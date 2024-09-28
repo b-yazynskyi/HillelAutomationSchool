83f33e92fcebcbf3e1508fed0fcc5489f0ddcc89
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-28 11:42:11 +0300

    Add implementation of zipZap to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,20 @@ import utils.Utils;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            System.out.println(Utils.repeatEnd(Utils.getStringFromUser(), Utils.getNumberFromUser()));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+//        try {
+//            System.out.println(Utils.repeatEnd(Utils.getStringFromUser(), Utils.getNumberFromUser()));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        try {
+//            System.out.println(Utils.mixString(Utils.getStringFromUser(), Utils.getStringFromUser()));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        System.out.println(Utils.xyzMiddle(Utils.getStringFromUser()));
 
-        try {
-            System.out.println(Utils.mixString(Utils.getStringFromUser(), Utils.getStringFromUser()));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println(Utils.xyzMiddle(Utils.getStringFromUser()));
+        System.out.println(Utils.zipZap(Utils.getStringFromUser()));
     }
 }
