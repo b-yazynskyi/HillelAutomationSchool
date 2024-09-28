9174ac0d8303884e9df628d3f5c813109504abca
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-28 11:53:58 +0300

    Add comments with task numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,27 @@ import utils.Utils;
 public class Main {
     public static void main(String[] args) {
 
+        // Task 7.1
         try {
             System.out.println(Utils.repeatEnd(Utils.getStringFromUser(), Utils.getNumberFromUser()));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
+        // Task 7.2
         try {
             System.out.println(Utils.mixString(Utils.getStringFromUser(), Utils.getStringFromUser()));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
 
+        // Task 7.3
         System.out.println(Utils.xyzMiddle(Utils.getStringFromUser()));
 
+        // Task 7.4
         System.out.println(Utils.zipZap(Utils.getStringFromUser()));
 
+        // Task 7.5
         System.out.println(Utils.xyzThere(Utils.getStringFromUser()));
     }
 }
