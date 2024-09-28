534305e14de94862c7265271271d8d0669f8e225
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-28 12:12:53 +0300

    Create unit tests for repeatEnd method

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -8,4 +8,19 @@ public class UtilsTest {
     void wordAndNumber3Returns3lastLetters3times() {
         Assert.assertEquals(Utils.repeatEnd("Hello", 3), "llollollo");
     }
+
+    @Test(groups = {"repeatEnd"})
+    void wordAndNumber2Returns2lastLetters2times() {
+        Assert.assertEquals(Utils.repeatEnd("Hello", 2), "lolo");
+    }
+
+    @Test(groups = {"repeatEnd"})
+    void wordAndNumber1Returns1lastLetters1time() {
+        Assert.assertEquals(Utils.repeatEnd("Hello", 1), "o");
+    }
+
+    @Test(groups = {"repeatEnd"})
+    void wordLowerThanNumberShouldBeException() {
+        Assert.assertThrows(IllegalArgumentException.class, () -> Utils.repeatEnd("Hello", 6));
+    }
 }
