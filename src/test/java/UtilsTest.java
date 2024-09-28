f9ce8166d2383bd0f4e04eae503a8fa53517e076
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-09-28 12:06:43 +0300

    create wordAndNumber3Returns3lastLetters3times test

--- src/test/java/UtilsTest.java
+++ src/test/java/UtilsTest.java
@@ -1,2 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.Utils;
+
 public class UtilsTest {
+
+    @Test(groups = {"repeatEnd"})
+    void wordAndNumber3Returns3lastLetters3times() {
+        Assert.assertEquals(Utils.repeatEnd("Hello", 3), "llollollo");
+    }
 }
