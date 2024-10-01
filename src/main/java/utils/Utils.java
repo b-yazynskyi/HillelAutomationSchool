7e5293662aaf4710866f374c77ab97b5bb85250a
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-01 10:26:32 +0300

    Create catDog method in Utils

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,8 +1,16 @@
 package utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class Utils {
 
     private Utils() {}
 
+    public static boolean catDog(String word) {
+        int catCount = word.split("cat", -1).length - 1;
+        int dogCount = word.split("dog", -1).length - 1;
 
+        return catCount == dogCount;
+    }
 }
