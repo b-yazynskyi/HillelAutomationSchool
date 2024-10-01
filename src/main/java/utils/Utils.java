69ca01134f7ae0a75e31f87ff2d02dbacbeccaa6
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-01 10:27:46 +0300

    Create getStringFromUSer method

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,11 +1,18 @@
 package utils;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public final class Utils {
 
     private Utils() {}
+
+    public static String getStringFromUser() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.nextLine();
+    }
 
     public static boolean catDog(String word) {
         int catCount = word.split("cat", -1).length - 1;
