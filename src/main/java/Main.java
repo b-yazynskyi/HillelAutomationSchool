df8b55e095a758fe731923c61498dc31bdab5ebb
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-01 10:29:26 +0300

    Add implementation of catDog method to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import utils.Utils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 8.1
+        System.out.println(Utils.catDog(Utils.getStringFromUser()));
     }
 }
