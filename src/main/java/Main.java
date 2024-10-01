ff12895e1509868ed023d093de556e277117e072
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-01 11:15:03 +0300

    Add implementation of centeredAverage method in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 import utils.Utils;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
         // Task 8.1
         System.out.println(Utils.catDog(Utils.getStringFromUser()));
+
+        // Task 8.2
+        try {
+            System.out.println(Utils.centeredAverage(Utils.getArrayOfNumbersFromUser()));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
