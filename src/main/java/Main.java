60e9a246629ba722c2d37b9b16179cd45e0e141b
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-06 15:39:40 +0300

    Create implementation of sumIgnoreSections method in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,8 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
+        // Task 8.3
+        System.out.println(Utils.sumIgnoreSections(Utils.getArrayOfNumbersFromUser()));
 
     }
 }
