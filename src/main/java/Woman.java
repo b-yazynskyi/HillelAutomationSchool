7802ba13af42c2e3e052a7da6e4ab6cf717036e1
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-10 16:38:39 +0300

    Added getGender method

--- src/main/java/Woman.java
+++ src/main/java/Woman.java
@@ -1,8 +1,11 @@
 public class Woman extends Person {
-    public static final String sex = "woman";
-
+    public static final String GENDER = "жінка";
 
     public Woman(String name, int age, PersonRole profession) {
         super(name, age, profession);
+    }
+
+    public String getGender() {
+        return GENDER;
     }
 }
