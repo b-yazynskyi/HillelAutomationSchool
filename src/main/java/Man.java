7d110eca685d39385a295c1997513bc15df39f8b
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-10 15:48:12 +0300

    Add sex field to Man class

--- src/main/java/Man.java
+++ src/main/java/Man.java
@@ -1,4 +1,5 @@
 public class Man extends Person {
+    public static final String sex = "man";
 
     public Man(String name, int age, PersonRole profession) {
         super(name, age, profession);
