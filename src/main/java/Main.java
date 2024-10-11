8024449dff0bc416cbcf2bd24bc2db34a19f212e
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-11 10:38:33 +0300

    Fix call to PersonRole enum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Man("John", 25, Person.PersonRole.ENGINEER);
-        Person person2 = new Woman("Mary", 25, Person.PersonRole.TEACHER);
-        Person person3 = new Man("Bob", 35, Person.PersonRole.DOCTOR);
-        Person person4 = new Woman("Alice", 28, Person.PersonRole.ARCHITECT);
+        Person person1 = new Man("John", 25, PersonRole.ENGINEER);
+        Person person2 = new Woman("Mary", 25, PersonRole.TEACHER);
+        Person person3 = new Man("Bob", 35, PersonRole.DOCTOR);
+        Person person4 = new Woman("Alice", 28, PersonRole.ARCHITECT);
 
         person1.displayInformation();
         person2.displayInformation();
         person3.displayInformation();
         person4.displayInformation();
 
-        person4.setProfession(Person.PersonRole.DESIGNER);
+        person4.setProfession(PersonRole.DESIGNER);
 
         System.out.println("(Після оновлення професії)");
 
         person4.displayInformation();
 
         person1.displayInformation("Anton");
-        person2.displayInformation("Julia", Person.PersonRole.STUDENT);
+        person2.displayInformation("Julia", PersonRole.STUDENT);
 
     }
 }
