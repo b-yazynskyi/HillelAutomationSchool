b3062bb1860b3e3015531b2a0666676f55874ae4
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-11 10:38:03 +0300

    Remove PersonRole enum from class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -43,26 +43,4 @@ public abstract class Person implements Displayable {
                 ", Вік: " + age +
                 ", Професія: " + profession;
     }
-
-    public enum PersonRole {
-        STUDENT("Студент"),
-        TEACHER("Вчитель"),
-        DOCTOR("Лікар"),
-        ENGINEER("Інженер"),
-        ARTIST("Артист"),
-        DESIGNER("Дизайнер"),
-        ARCHITECT("Архітектор"),
-        OTHER("Інший");
-
-        private final String name;
-
-        PersonRole(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
 }
