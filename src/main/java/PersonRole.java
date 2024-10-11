b71063a337fd231c7acf4e7f8067fa96cce34b44
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-11 10:38:20 +0300

    Create new enum separated from class Person

--- src/main/java/PersonRole.java
+++ src/main/java/PersonRole.java
@@ -1,8 +1,21 @@
 public enum PersonRole {
-    STUDENT,
-    TEACHER,
-    DOCTOR,
-    ENGINEER,
-    ARTIST,
-    OTHER
+    STUDENT("Студент"),
+    TEACHER("Вчитель"),
+    DOCTOR("Лікар"),
+    ENGINEER("Інженер"),
+    ARTIST("Артист"),
+    DESIGNER("Дизайнер"),
+    ARCHITECT("Архітектор"),
+    OTHER("Інший");
+
+    private final String name;
+
+    PersonRole(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
