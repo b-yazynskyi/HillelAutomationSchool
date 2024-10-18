5e9878fd3f4901bca7509d03f5daad5921c77e86
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-18 11:38:11 +0300

    Add implementation of createMap method in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import utils.Util;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println(Util.getLengthOfStrings(Util.getStringsArrayFromUser()));
+        System.out.println(Util.createMap(Util.getStringsArrayFromUser()));
     }
 }
