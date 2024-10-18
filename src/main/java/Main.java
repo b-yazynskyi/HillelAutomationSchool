68d1b3a853ae2736865e2d81aa4cd575121d694e
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-10-18 10:26:50 +0300

    Add implementation of getLengthOfStrings method in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import utils.Util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Util.getLengthOfStrings(Util.getStringsArrayFromUser()));
     }
 }
