6738498e695c67c3f9db41be790bec87a197deef
Язинський Богдан <b.yazynskyi@bankvostok.com.ua>
2024-10-25 14:53:31 +0300

    Refactor main method to call each creating object separately

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,45 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Person person1 = new Man(null,25, PersonRole.ENGINEER);
-            Person person2 = new Woman("Mary", 25, PersonRole.TEACHER);
-            Person person3 = new Man("Bob", 35, PersonRole.DOCTOR);
-            Person person4 = new Woman("Alice", 28, PersonRole.ARCHITECT);
+            try {
+                Person person1 = new Man(null,25, PersonRole.ENGINEER);
+                person1.displayInformation();
+                System.out.println("Display person1 object with new name:");
+                person1.displayInformation("Anton");
+            } catch (IllegalArgumentException | NullNameException e) {
+                System.out.println(e.getMessage());
+            }
 
-            person1.displayInformation();
-            person2.displayInformation();
-            person3.displayInformation();
-            person4.displayInformation();
+            try {
+                Person person2 = new Woman("Mary", 25, PersonRole.TEACHER);
+                person2.displayInformation();
+                System.out.println("Display person2 object with new name:");
+                person2.displayInformation("Julia", PersonRole.STUDENT);
+            } catch (IllegalArgumentException | NullNameException e) {
+                System.out.println(e.getMessage());
+            }
 
-            person4.setProfession(PersonRole.DESIGNER);
+            try {
+                Person person3 = new Man("Bob", 35, PersonRole.DOCTOR);
+                person3.displayInformation();
+            } catch (IllegalArgumentException | NullNameException e) {
+                System.out.println(e.getMessage());
+            }
 
-            System.out.println("(Після оновлення професії)");
+            try {
+                Person person4 = new Woman("Alice", 28, PersonRole.ARCHITECT);
+                person4.displayInformation();
 
-            person4.displayInformation();
+                person4.setProfession(PersonRole.DESIGNER);
 
-            person1.displayInformation("Anton");
-            person2.displayInformation("Julia", PersonRole.STUDENT);
-        } catch (IllegalArgumentException | NullNameException e) {
+                System.out.println("(Після оновлення професії)");
+
+                person4.displayInformation();
+            } catch (IllegalArgumentException | NullNameException e) {
+                System.out.println(e.getMessage());
+            }
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
