7020f087241958b8683e51889d4484e1df830b10
Язинський Богдан <b.yazynskyi@bankvostok.com.ua>
2024-10-25 14:37:37 +0300

    Create own exception class NullNameException

---
+++ src/main/java/NullNameException.java
@@ -0,0 +1,8 @@
+public final class NullNameException extends RuntimeException {
+
+    public NullNameException() {}
+
+    public NullNameException(String message) {
+        super(message);
+    }
+}
