14cdf9b98ea1caf368d8b4a919e5536b0fb6a42f
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-05 17:03:23 +0200

    Add clickGuestLogInButton method to MainPage

---
+++ src/main/java/pages/MainPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Objects;
+
+public class MainPage extends BasePage{
+    private WebElement guestLogInButton;
+
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickGuestLogInButton() {
+        if (Objects.isNull(guestLogInButton)) {
+            guestLogInButton = getDriver().findElement(By.xpath(GUEST_LOG_IN_BUTTON));
+        }
+
+        guestLogInButton.click();
+    }
+
+
+}
