2d91091714b6cd45634a4ced8e50d98783d79dbd
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-05 17:09:04 +0200

    Add selectors for links from border menu

--- src/main/java/pages/Page.java
+++ src/main/java/pages/Page.java
@@ -6,4 +6,8 @@ public interface Page {
     String BASE_URL = "https://guest:welcome2qauto@qauto.forstudy.space/";
 
     String GUEST_LOG_IN_BUTTON = "//button[@class=\"header-link -guest\"]";
+    String GARAGE_BUTTON = "//a[@routerLink='garage']";
+    String FUEL_EXPENSES_BUTTON = "//a[@routerLink='expenses']";
+    String INSTRUCTIONS_BUTTON = "//a[@routerLink='instructions']";
+    String LOG_OUT_BUTTON = "//a[@class='btn btn-link text-danger btn-sidebar sidebar_btn']";
 }
