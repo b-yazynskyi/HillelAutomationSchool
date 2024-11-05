4d8c7056a6fb4e4a4baada1fef09164467f4611b
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-05 17:18:27 +0200

    Create PanelPageSelectors

---
+++ src/main/java/pages/PanelPageSelectors.java
@@ -0,0 +1,8 @@
+package pages;
+
+public interface PanelPageSelectors {
+    String GARAGE_BUTTON = "//a[@routerLink='garage']";
+    String FUEL_EXPENSES_BUTTON = "//a[@routerLink='expenses']";
+    String INSTRUCTIONS_BUTTON = "//a[@routerLink='instructions']";
+    String LOG_OUT_BUTTON = "//a[@class='btn btn-link text-danger btn-sidebar sidebar_btn']";
+}
