ebec4c97a4e3bc2ae8917ddcd59f23ba7b54ae36
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-05 17:05:53 +0200

    Create PanelPage class

---
+++ src/main/java/pages/PanelPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PanelPage extends BasePage{
+
+    public PanelPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
