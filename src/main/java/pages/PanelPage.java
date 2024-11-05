78af250a12eb0abca7339874bc9b77eb204cf3cb
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-05 17:17:20 +0200

    Add elements fields and method to getText

--- src/main/java/pages/PanelPage.java
+++ src/main/java/pages/PanelPage.java
@@ -1,11 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.Objects;
 
 public class PanelPage extends BasePage{
+    private WebElement garageButton;
+    private WebElement fuelExpensesButton;
+    private WebElement instructionsButton;
 
     public PanelPage(WebDriver driver) {
         super(driver);
+    }
+
+    public String getTextOfGarageButton() {
+        if (Objects.isNull(garageButton)) {
+            garageButton = getDriver().findElement(By.xpath(GARAGE_BUTTON));
+        }
+
+        return garageButton.getText();
     }
 
 
