712cb780ba0760b40eaea3c2c1a068fabe82a26a
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 07:46:01 +0200

    Delete methods for every button and made 1 unified method

--- src/main/java/pages/PanelPage.java
+++ src/main/java/pages/PanelPage.java
@@ -4,51 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
 import java.util.Objects;
 
 import static pages.PanelPageSelectors.*;
 
-public class PanelPage extends BasePage{
+public class PanelPage extends BasePage {
     private static final String URL = "/panel";
-    private WebElement garageButton;
-    private WebElement fuelExpensesButton;
-    private WebElement instructionsButton;
-    private WebElement logOutButton;
+    private List<WebElement> borderMenuButtons;
 
     public PanelPage(WebDriver driver) {
         super(driver);
     }
 
-    public String getTextOfGarageButton() {
-        if (Objects.isNull(garageButton)) {
-            garageButton = getDriver().findElement(By.xpath(GARAGE_BUTTON));
+    /* До цього робив варіант щоб під кожну кнопку був метод який витягає текст по цій кнопці,
+    але вирішив зробити 1 уніфікований метод так як не має потреби у подальшій взаємодії з кожною кнопкою окремо
+    */
+    public List<String> getBorderMenuButtonsText() {
+        if (Objects.isNull(borderMenuButtons)) {
+            borderMenuButtons = getDriver().findElements(By.xpath(BORDER_MENU_BUTTONS));
         }
 
-        return garageButton.getText();
-    }
-
-    public String getTextOfFuelExpensesButton() {
-        if (Objects.isNull(fuelExpensesButton)) {
-            fuelExpensesButton = getDriver().findElement(By.xpath(FUEL_EXPENSES_BUTTON));
-        }
-
-        return fuelExpensesButton.getText();
-    }
-
-    public String getTextOfInstructionsButton() {
-        if (Objects.isNull(instructionsButton)) {
-            instructionsButton = getDriver().findElement(By.xpath(INSTRUCTIONS_BUTTON));
-        }
-
-        return instructionsButton.getText();
-    }
-
-    public String getTextOfLogOutButton() {
-        if (Objects.isNull(logOutButton)) {
-            logOutButton = getDriver().findElement(By.xpath(LOG_OUT_BUTTON));
-        }
-
-        return logOutButton.getText();
+        return borderMenuButtons.stream().filter(Objects::nonNull).map(WebElement::getText).toList();
     }
 
     public PanelPage openPage() {
