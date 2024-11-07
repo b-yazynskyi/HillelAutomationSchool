e74be1454c1ed6602e992e15827f14254e713d44
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 07:46:11 +0200

    Change calling unified method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.MainPage;
+import pages.PanelPage;
+
+import java.time.Duration;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        MainPage mainPage = new MainPage(driver);
+        PanelPage panelPage = mainPage.openPage().clickGuestLogInButton();
+
+        List<String> borderMenuButtonsText = panelPage.getBorderMenuButtonsText();
+
+        borderMenuButtonsText.forEach(System.out::println);
+
+        driver.quit();
     }
 }
