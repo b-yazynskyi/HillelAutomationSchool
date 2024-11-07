0929aeec990d96825fa5aa3b990324b628544020
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 19:08:03 +0200

    Add explicity constructor to BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -4,6 +4,8 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class BasePage {
 
+    public BasePage() {}
+
     public static void navigateToPage(String url) {
         open(url);
     }
