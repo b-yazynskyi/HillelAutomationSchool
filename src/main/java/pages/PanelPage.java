1fb342548198ddaf7724902728f98af414f1cb0a
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 18:10:19 +0200

    Create method to get text from buttons

--- src/main/java/pages/PanelPage.java
+++ src/main/java/pages/PanelPage.java
@@ -1,4 +1,18 @@
 package pages;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.$$x;
+
 public class PanelPage {
+    private final ElementsCollection BORDER_MENU_BUTTONS =
+            $$x("//*[contains(@class, \"btn-sidebar sidebar_btn\")]");
+
+    public List<String> getTextOfBorderMenuButtons() {
+        return BORDER_MENU_BUTTONS.stream().filter(Objects::nonNull).map(SelenideElement::getText).toList();
+    }
 }
