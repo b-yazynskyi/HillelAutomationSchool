04ecfcce0a9b924108b671dbf4d7e26fd9a35460
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 17:22:13 +0200

    Add selector for guest log in button + method to click on it

--- src/main/java/pages/QaAutoForStudyPage.java
+++ src/main/java/pages/QaAutoForStudyPage.java
@@ -1,4 +1,15 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.page;
+
 public class QaAutoForStudyPage {
+    private final SelenideElement GUEST_LOG_IN_BUTTON = $x("//button[@class='header-link -guest']");
+
+    public PanelPage clickGuestLogInPage() {
+        GUEST_LOG_IN_BUTTON.click();
+        return page(PanelPage.class);
+    }
 }
