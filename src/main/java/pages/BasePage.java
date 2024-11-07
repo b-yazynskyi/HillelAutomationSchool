e5f68e9bab21d4b297dd3343a702d4aa1379c005
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 18:19:44 +0200

    Add method to navigate on basePage class

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,10 @@
 package pages;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class BasePage {
+
+    public static void navigateToPage(String url) {
+        open(url);
+    }
 }
