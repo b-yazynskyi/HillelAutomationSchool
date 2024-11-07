e523e62590b78d0f53980c7be068e6ea258e8fe8
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-07 18:50:31 +0200

    Add constant for headless mode

--- src/main/java/utils/AppConstants.java
+++ src/main/java/utils/AppConstants.java
@@ -3,4 +3,5 @@ package utils;
 public class AppConstants {
 
     public static final String BASE_URL = "https://guest:welcome2qauto@qauto.forstudy.space/";
+    public static final boolean HEADLESS_PROP_MODE = true;
 }
