852b45a5939c832bc25978f49034b503011d4896
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-09 20:25:39 +0200

    Create add and subtract methods

--- src/main/java/utils/Calculator.java
+++ src/main/java/utils/Calculator.java
@@ -4,5 +4,41 @@ public final class Calculator {
 
     private Calculator() {}
 
+    public static int add(int... numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+
+        return result;
+    }
+
+    public static double add(double... numbers) {
+        double result = 0;
+        for (double number : numbers) {
+            result += number;
+        }
+
+        return result;
+    }
+
+    public static int subtract(int... numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result -= number;
+        }
+
+        return result;
+    }
+
+    public static double subtract(double... numbers) {
+        double result = 0;
+        for (double number : numbers) {
+            result -= number;
+        }
+
+        return result;
+    }
+
 
 }
