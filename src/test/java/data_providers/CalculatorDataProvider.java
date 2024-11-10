d09794b7a36ba7f55519ae50d6c7602f1362f1f9
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-10 09:39:00 +0200

    Created subtractCalculatorDataProvider method with text data

--- src/test/java/data_providers/CalculatorDataProvider.java
+++ src/test/java/data_providers/CalculatorDataProvider.java
@@ -4,11 +4,20 @@ import org.testng.annotations.DataProvider;
 
 public class CalculatorDataProvider {
     @DataProvider
-    public static Object[][] addCalculatorDataProvider() {
+    public Object[][] addCalculatorDataProvider() {
         return new Object[][] {
                 {10, 5, 5},
                 {8, 2, 6},
                 {9000, 6000, 2000, 999, 1}
+        };
+    }
+
+    @DataProvider
+    public Object[][] subtractCalculatorDataProvider() {
+        return new Object[][] {
+                {5, 15, 10},
+                {500, 1000, 200, 300},
+                {1.87, 5, 3.13}
         };
     }
 }
