1e1aedcc4906cde4d1ef2b2b30ae18831f4242f5
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-10 07:42:50 +0200

    Create calculatorDataProvider method

--- src/test/java/data_providers/CalculatorDataProvider.java
+++ src/test/java/data_providers/CalculatorDataProvider.java
@@ -1,4 +1,8 @@
 package data_providers;
 
+import org.testng.annotations.DataProvider;
+
 public class CalculatorDataProvider {
+    @DataProvider
+    public static Object[][] calculatorDataProvider()
 }
