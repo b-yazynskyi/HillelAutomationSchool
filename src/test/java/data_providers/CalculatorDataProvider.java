2a0f62a59d4f3ef8e3ebd9b8ae4f2c528763d202
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-10 07:58:58 +0200

    Create data provider method with test data for add method

--- src/test/java/data_providers/CalculatorDataProvider.java
+++ src/test/java/data_providers/CalculatorDataProvider.java
@@ -4,5 +4,11 @@ import org.testng.annotations.DataProvider;
 
 public class CalculatorDataProvider {
     @DataProvider
-    public static Object[][] calculatorDataProvider()
+    public static Object[][] addCalculatorDataProvider() {
+        return new Object[][] {
+                {10, 5, 5},
+                {8, 2, 6},
+                {9000, 6000, 2000, 999, 1}
+        };
+    }
 }
