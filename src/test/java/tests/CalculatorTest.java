c31d41769d2c31ee97cc9bade8859634675c4487
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-10 09:28:03 +0200

    Create BeforeClass method where initiate objects and AfterClass method where make null objects

--- src/test/java/tests/CalculatorTest.java
+++ src/test/java/tests/CalculatorTest.java
@@ -2,16 +2,29 @@ package tests;
 
 import data_providers.CalculatorDataProvider;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import utils.Calculator;
 
 public class CalculatorTest extends BaseTest {
+    private Calculator calculator;
+
+    @BeforeClass
+    public void initiateObjects() {
+        calculator = new Calculator();
+    }
 
     @Test(priority = 1, dataProviderClass = CalculatorDataProvider.class, dataProvider = "addCalculatorDataProvider",
         description = "Test where we adding some amount of numbers and expect this data")
     public void testAddition(double expectedResult, double... numbers) {
-        double result = Calculator.add(numbers);
+        double result = calculator.add(numbers);
 
         Assert.assertEquals(result, expectedResult);
+    }
+
+    @AfterClass
+    public void closeObjects() {
+        calculator = null;
     }
 }
