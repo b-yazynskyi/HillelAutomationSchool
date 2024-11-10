5a9f6f179363c85203e2411f78a71c7f7119256c
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-10 07:33:03 +0200

    Delete int methods

--- src/main/java/utils/Calculator.java
+++ src/main/java/utils/Calculator.java
@@ -4,28 +4,10 @@ public final class Calculator {
 
     private Calculator() {}
 
-    public static int add(int... numbers) {
-        int result = 0;
-        for (int number : numbers) {
-            result += number;
-        }
-
-        return result;
-    }
-
     public static double add(double... numbers) {
         double result = 0;
         for (double number : numbers) {
             result += number;
-        }
-
-        return result;
-    }
-
-    public static int subtract(int... numbers) {
-        int result = 0;
-        for (int number : numbers) {
-            result -= number;
         }
 
         return result;
@@ -40,36 +22,10 @@ public final class Calculator {
         return result;
     }
 
-    public static int multiply(int... numbers) {
-        int result = 1;
-        for (int number : numbers) {
-            result *= number;
-        }
-
-        return result;
-    }
-
     public static double multiply(double... numbers) {
         double result = 1;
         for (double number : numbers) {
             result *= number;
-        }
-
-        return result;
-    }
-
-    public static int divide(int... numbers) throws ArithmeticException {
-        if (numbers.length == 0) {
-            throw new IllegalArgumentException("At least 1 number must be");
-        }
-
-        int result = numbers[0];
-
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] == 0) {
-                throw new ArithmeticException("The divisor 0 is impossible");
-            }
-            result /= numbers[i];
         }
 
         return result;
@@ -84,7 +40,7 @@ public final class Calculator {
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] == 0) {
-                throw new ArithmeticException("The divisor 0 is impossible")
+                throw new ArithmeticException("The divisor 0 is impossible");
             }
 
         }
