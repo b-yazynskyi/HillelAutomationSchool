10bd1c205c8ee2e591a288f5f467c615bf439cee
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-11 08:47:44 +0200

    Create test for testZeroDivision

--- src/test/java/tests/CalculatorTest.java
+++ src/test/java/tests/CalculatorTest.java
@@ -48,6 +48,28 @@ public class CalculatorTest extends BaseTest {
         Assert.assertEquals(result, expectedResult);
     }
 
+    @Test(priority = 1, dataProviderClass = CalculatorDataProvider.class,
+            dataProvider = "divideCalculatorDataProvider",
+            description = "Test where we diving some amount of numbers"
+    )
+    public void testDivision(double expectedResult, double... numbers) {
+        double result = calculator.divide(numbers);
+
+        System.out.println("The result of division numbers: " + Arrays.toString(numbers) + " is " + result);
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @Test(priority = 1, expectedExceptions = ArithmeticException.class,
+            dataProviderClass = CalculatorDataProvider.class,
+            dataProvider = "zeroDivisionCalculatorDataProvider",
+            description = "Negative test to check exception when we try divide for 0"
+    )
+    public void testZeroDivision(double... numbers) {
+        calculator.divide(numbers);
+
+        System.out.println("The result of division numbers: " + Arrays.toString(numbers) + " is ArithmeticException");
+    }
+
     @AfterClass
     public void closeObjects() {
         calculator = null;
