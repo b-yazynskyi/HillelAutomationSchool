e77ac053e4cdfc808db4519aa8df5453d6fa5b82
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-12 16:49:17 +0200

    Create class with AppConstants

---
+++ src/main/java/utils/AppConstants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class AppConstants {
+    public static final boolean HEADLESS_APP_MODE = true;
+    public static final String BASE_URL = "https://guest:welcome2qauto@qauto.forstudy.space/";
+}
