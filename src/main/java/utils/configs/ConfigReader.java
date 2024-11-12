d9eab00e6e04012756326727aca05b6f0ff08310
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-12 16:59:16 +0200

    Create ConfigReader class

---
+++ src/main/java/utils/configs/ConfigReader.java
@@ -0,0 +1,33 @@
+package utils.configs;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static final String CONFIG_FILE = "/app.config";
+    private static ConfigReader instance = null;
+    private Properties properties;
+
+    public synchronized static ConfigReader getInstance() {
+        if (instance == null) {
+            instance = new ConfigReader();
+        }
+
+        return instance;
+    }
+
+    private ConfigReader() {
+        properties = new Properties();
+
+        try {
+            properties.load(ConfigReader.class.getResourceAsStream(CONFIG_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String loadConfig(String name) {
+        return getInstance().properties.getProperty(name);
+    }
+
+}
