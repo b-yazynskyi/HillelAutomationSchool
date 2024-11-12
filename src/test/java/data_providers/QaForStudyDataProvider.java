f94c47d6bfc030b6b122c7632e70adeed52fe9c5
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-12 17:11:53 +0200

    Create data provider for QAAutoTest

---
+++ src/test/java/data_providers/QaForStudyDataProvider.java
@@ -0,0 +1,12 @@
+package data_providers;
+
+import org.testng.annotations.DataProvider;
+
+public class QaForStudyDataProvider {
+    @DataProvider
+    public Object[] backGroundColorDataProvider() {
+        return new Object[] {
+                "#0275d8"
+        };
+    }
+}
