a8a9be34940fbdfe8b4839000cbd7bb6e0520d87
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-14 08:55:01 +0200

    Add Step annotation for navigate to page

--- src/main/java/pages/basePage/BasePage.java
+++ src/main/java/pages/basePage/BasePage.java
@@ -1,9 +1,12 @@
 package pages.basePage;
 
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selenide.open;
 
 public abstract class BasePage {
 
+    @Step("Open the website")
     public static void navigateToPage(String url) {
         open(url);
     }
