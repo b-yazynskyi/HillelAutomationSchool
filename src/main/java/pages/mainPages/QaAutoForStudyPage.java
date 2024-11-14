1d36aeadb316f98511d2df429c5bb252dd514ed9
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-14 08:55:13 +0200

    Add Step annotation for methods

--- src/main/java/pages/mainPages/QaAutoForStudyPage.java
+++ src/main/java/pages/mainPages/QaAutoForStudyPage.java
@@ -1,6 +1,7 @@
 package pages.mainPages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.Color;
 import pages.basePage.BasePage;
 
@@ -10,10 +11,12 @@ public class QaAutoForStudyPage extends BasePage {
     private final SelenideElement LOGO = $x("//*[@class='header_logo']");
     private final SelenideElement SIGN_UP = $x("//*[@class='hero-descriptor_btn btn btn-primary']");
 
+    @Step("Check is logo on site visible")
     public boolean isLogoVisible() {
         return LOGO.isDisplayed();
     }
 
+    @Step("Getting background color of Sign Up button")
     public String getBackGroundColorForSignUp() {
         String backGroundColor = SIGN_UP.getCssValue("background-color");
         return Color.fromString(backGroundColor).asHex();
