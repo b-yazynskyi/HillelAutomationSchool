eed86f9d64e0121d405b2cd10544a8533eaee8ca
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-22 08:50:20 +0200

    Created tests for checking Logo display status and Sihn Up button backgroudn color

---
+++ src/test/java/tests/QaForStudySpacePageTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import dataProviders.QaForStudySpacePageDataProvider;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.QaForStudySpacePage;
+
+public class QaForStudySpacePageTest {
+    private QaForStudySpacePage qaForStudySpacePage;
+
+    @BeforeMethod
+    public void initPages() {
+        qaForStudySpacePage = QaForStudySpacePage.openPage();
+    }
+
+    @Test(testName = "IsLogoDisplayed", description = "Check either Logo displayed or not")
+    public void logoDisplayedTest() {
+        Assert.assertTrue(qaForStudySpacePage.checkIsLogoDisplayed(), "Logo does not displayed");
+    }
+
+    @Test(testName = "SignUpButtonBackgroundColor",
+            description = "Check either background color of SignUp button proper or not",
+            dataProviderClass = QaForStudySpacePageDataProvider.class,
+            dataProvider = "backGroundColorDataProvider"
+    )
+    public void signUpButtonBackgroundColorTest(String expectedBackGroundColor) {
+        String actualBackGroundColor = qaForStudySpacePage.getBackGroundColorOfSignUp();
+
+        Assert.assertEquals(actualBackGroundColor, expectedBackGroundColor,
+                "Background color of Sign up button is incorrect");
+    }
+}
