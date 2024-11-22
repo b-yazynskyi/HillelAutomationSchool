ee5ed4bb5655235170d389f57692a83f7113adec
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-22 08:32:36 +0200

    Created QaForStudySpacePage class with methods to get logo and background color of Sign Up button

---
+++ src/main/java/pages/QaForStudySpacePage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.Color;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class QaForStudySpacePage {
+    private final SelenideElement LOGO_PAGE = $x("//*[@class='header_left d-flex align-items-center']");
+    private final SelenideElement SIGN_UP_BUTTON = $x("//*[@class='hero-descriptor_btn btn btn-primary']");
+
+    public boolean checkIsLogoDisplayed() {
+        LOGO_PAGE.shouldBe(Condition.visible);
+
+        return LOGO_PAGE.isDisplayed();
+    }
+
+    public String getBackGroundColorOfSignUp() {
+        SIGN_UP_BUTTON.shouldHave(Condition.cssValue("background-color", "#0275d8"));
+
+        return Color.fromString(SIGN_UP_BUTTON.getCssValue("background-color")).toString();
+    }
+}
