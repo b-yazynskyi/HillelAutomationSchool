0420a7cfee553b22d3157b630228434a7937026d
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-25 20:34:43 +0200

    Add new dataprovider for socialNetworkNamesAndUrls

--- src/test/java/dataProviders/QaForStudySpacePageDataProvider.java
+++ src/test/java/dataProviders/QaForStudySpacePageDataProvider.java
@@ -3,6 +3,8 @@ package dataProviders;
 
 import org.testng.annotations.DataProvider;
 
+import java.util.Map;
+
 public class QaForStudySpacePageDataProvider {
     @DataProvider
     public Object[] backGroundColorDataProvider() {
@@ -16,5 +18,21 @@ public class QaForStudySpacePageDataProvider {
         return new Object[] {
                 "Hillel IT School | Учись ради мечты! - YouTube"
         };
+    }
+
+    @DataProvider
+    public Object[][] socialNetworkNamesAndUrls() {
+        Map<String, String> expectedLinks = Map.of(
+                "facebook", "https://www.facebook.com/Hillel.IT.School",
+                "telegram", "https://t.me/ithillel_kyiv",
+                "youtube", "https://www.youtube.com/user/HillelITSchool",
+                "instagram", "https://www.instagram.com/hillel_itschool/",
+                "linkedin", "https://www.linkedin.com/school/ithillel/"
+        );
+
+        return expectedLinks.entrySet()
+                .stream()
+                .map(entry -> new Object[]{entry.getKey(), entry.getValue()})
+                .toArray(Object[][]::new);
     }
 }
