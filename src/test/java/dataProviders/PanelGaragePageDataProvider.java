9444dfacaa481642abaf3111037f1ae9507e22af
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-26 20:54:59 +0200

    Add info to data provider

--- src/test/java/dataProviders/PanelGaragePageDataProvider.java
+++ src/test/java/dataProviders/PanelGaragePageDataProvider.java
@@ -6,10 +6,10 @@ public class PanelGaragePageDataProvider {
     @DataProvider
     public Object[][] dataForAddNewCarIntoGarage() {
         return new Object[][] {
-                {"Audi", "TT", "20"},
-                {"BMW", "3", "90"},
-                {"Ford", "Fiesta", "1"},
-                {"Porsche", "911", "45"}
+                {"Audi", "TT", "20", "audi.png"},
+                {"BMW", "3", "90", "bmw.png"},
+                {"Ford", "Fiesta", "1", "ford.png"},
+                {"Porsche", "911", "45", "porsche.png"}
         };
     }
 }
