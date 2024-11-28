a24cb3b38bf47f9d016d78ab51359423f12eaadf
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-28 15:05:33 +0200

    Remove unused import

--- src/main/java/pages/PanelGaragePage.java
+++ src/main/java/pages/PanelGaragePage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
