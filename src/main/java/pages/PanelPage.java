7c39ca357a7f6d91d7ae0ec555e6ebbfeea76f01
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-11-29 11:43:28 +0200

    Add element with instruction button + method to click on it and openPage

--- src/main/java/pages/PanelPage.java
+++ src/main/java/pages/PanelPage.java
@@ -1,4 +1,30 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+@Slf4j
 public class PanelPage {
+    private final SelenideElement INSTRUCTIONS_BUTTON = $x("//a[@routerLink='instructions']");
+
+    @Step("Click on Instructions button")
+    public PanelInstructionsPage clickOnInstructionsButton() {
+        log.info("Clicking on Instruction button");
+        INSTRUCTIONS_BUTTON.shouldBe(Condition.clickable).click();
+
+        return new PanelInstructionsPage();
+    }
+
+    @Step("Open PanelPage")
+    public PanelPage openPage() {
+        log.info("Opening PanelPage");
+        open("/panel");
+
+        return new PanelPage();
+    }
 }
