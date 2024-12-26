8941527f4b5ea3283cc16164bdcb419ae321584a
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-12-26 18:08:13 +0200

    Change hardcode to config properties

--- src/test/java/pages/BaseTest.java
+++ src/test/java/pages/BaseTest.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.net.NetworkUtils;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeMethod;
+import utils.AppConfigUtils;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -27,11 +28,19 @@ public abstract class BaseTest {
     }
 
     private void configBrowser() {
+        if (AppConfigUtils.CONFIG.isSelenoid()) {
+            Configuration.remote = AppConfigUtils.CONFIG.selenoidRemote();
+        }
+
+        if (AppConfigUtils.CONFIG.isHeadless()) {
+            Configuration.headless = true;
+        }
+
         Configuration.proxyEnabled = true;
         Configuration.proxyHost = new NetworkUtils().getNonLoopbackAddressOfThisMachine();
         Configuration.fileDownload = FileDownloadMode.PROXY;
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.browser = "chrome";
+
+        Configuration.browser = AppConfigUtils.CONFIG.browserName();
         Configuration.browserSize = "1920x1080";
         Configuration.screenshots = true;
         Configuration.savePageSource = true;
@@ -45,12 +54,12 @@ public abstract class BaseTest {
     private void configCapabilities() {
         var capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserName", AppConfigUtils.CONFIG.browserName());
         capabilities.setCapability("acceptInsecureCerts", true);
 
         var selenoidOptions = new HashMap<String, Object>();
-        selenoidOptions.put("enableVNC", true);
-        selenoidOptions.put("enableVideo", false);
+        selenoidOptions.put("enableVNC", AppConfigUtils.CONFIG.isVnc());
+        selenoidOptions.put("enableVideo", AppConfigUtils.CONFIG.isEnableVideo());
         selenoidOptions.put("videoName", "testVideo"
                 + RandomStringUtils.random(3, 0, 0, true, true, null
                 , Random.from(RandomGenerator.getDefault())) + ".mp4");
