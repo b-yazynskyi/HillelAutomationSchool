f9014d959fac3f6dfcfbed214319a95033e5283d
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-12-30 14:41:35 +0200

    Get a POJO model for Car response

--- api-hillel-tests/src/main/java/responses/GetCarBrandsResponse.java
+++ api-hillel-tests/src/main/java/responses/GetCarBrandsResponse.java
@@ -1,18 +1,18 @@
 package responses;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 public class GetCarBrandsResponse {
     private String status;
     private List<Brand> data;
 
-
+    @Data
     public static class Brand {
         private int id;
         private String title;
