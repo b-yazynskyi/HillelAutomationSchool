8866d1d62070c50ad2f9a2390361d65310fe5dcd
bohdanyazynskyi <bohdan.yazynskyi@amo.tech>
2024-12-30 14:49:13 +0200

    Refactored test to call auth more clearable

--- api-hillel-tests/src/test/java/api/authentication/AuthenticationTest.java
+++ api-hillel-tests/src/test/java/api/authentication/AuthenticationTest.java
@@ -1,19 +1,32 @@
 package api.authentication;
 
 import base.BaseTest;
-import io.restassured.RestAssured;
+import clients.AuthClient;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import responses.GetLogOutResponse;
 
-import static org.hamcrest.Matchers.equalTo;
+import static utils.JsonUtils.fromJson;
+
 
 public class AuthenticationTest extends BaseTest {
+    private AuthClient authClient;
+
+    @BeforeClass
+    public void initClients() {
+        authClient = new AuthClient();
+    }
 
     @Test
     public void logOutTest() {
-        RestAssured.when()
-                .get("https://qauto.forstudy.space/api/auth/logout")
-                .then()
-                .statusCode(200)
-                .body("status", equalTo("ok"));
+        var response = authClient.getLogOut();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(response.statusCode(), 200, "Status code should be 200");
+
+        GetLogOutResponse getLogOutResponse = fromJson(response.getBody().asString(), GetLogOutResponse.class);
+        softAssert.assertEquals(getLogOutResponse.getStatus(), "ok", "Status should be ok");
+        softAssert.assertAll();
     }
 }
